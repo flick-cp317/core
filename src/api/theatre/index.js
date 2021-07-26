07a847ee4e9983bdf510908c7cba208eabb09ab9
scott-w-baker <scottwbaker9@gmail.com>
2021-07-26 12:21:01 -0400

    added fuction to respond with array of theatres user is in

--- src/api/theatre/index.js
+++ src/api/theatre/index.js
@@ -10,6 +10,7 @@ theatreRouter.get('/', auth, async (req, res) => {
     return;
 });
 
+//responds with the name of a specific theatre
 theatreRouter.get('/:theatreid/name/', auth, async (req, res) => {
     const { theatreid } = req.params;
 
@@ -35,6 +36,41 @@ theatreRouter.get('/:theatreid/name/', auth, async (req, res) => {
     }
 
     res.status(200).json(result.rows[0].theatrename);
+
+    return;
+});
+
+//responds with an array of theatres the user is in and userid
+theatreRouter.get('/user/:userid/', auth, async (req, res) => {
+    const {userid} = req.params;
+    let sql, requestArray, result;
+
+    sql = await promises.readFile(
+        './src/db/sql/theatre/getUserTheatres.sql',
+        'utf-8',
+    );
+
+    requestArray = [ userid ];
+    
+    try {
+        result = await pool.query(sql, requestArray);
+    }
+    catch (error) {
+        res.status(400).json({});
+        return;
+    }
+
+    //code to create array for returning list of theatres
+    let response = {
+        userid: userid,
+        theatres: []
+    };
+
+    for (let i = 0; i < result.rows.length; i += 1) {
+        response.theatres.push(result.rows[i].theatreid);
+    }
+
+    res.status(200).json(response);
 
     return;
 });
