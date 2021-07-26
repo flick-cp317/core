0d6ae130c1df8045a17daa3140555be4c7935ed0
Alex D <dimi6520@mylaurier.ca>
2021-07-26 10:17:52 -0400

    Added movies to the file

--- src/assets/movies.js
+++ src/assets/movies.js
@@ -26,6 +26,27 @@ const Movies = [
         "description": "Historical events unfold from the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
         "genres": [ Genre.Drama, Genre.Romance ]
     },
+    {
+        "title": "Inception",
+        "description": "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+        "genres": [ Genre.Action, Genre.Adventure ] 
+    },
+    {
+        "title": "Once Upon a Time in Hollywood",
+        "description": "A faded television actor and his stunt double strive to achieve fame and success in the final years of Hollywood's Golden Age in 1969 Los Angeles.",
+        "genres": [ Genre.Comedy, Genre.Drama ] 
+    },
+    {
+        "title": "Casino Royale",
+        "description": "After earning 00 status and a licence to kill, Secret Agent James Bond sets out on his first mission as 007. Bond must defeat a private banker funding terrorists in a high-stakes game of poker at Casino Royale, Montenegro.",
+        "genres": [ Genre.Action, Genre.Adventure ] 
+    },
+    {
+        "title": "Moonrise Kingdom",
+        "description": "A pair of young lovers flee their New England town, which causes a local search party to fan out to find them.",
+        "genres": [ Genre.Comedy, Genre.Drama ] 
+    },
+
 ];
 
 export { Movies };
