8d602e0063a5aac3dde25756641d5ba5783cb7e5
Matthew Dibble <mdibble@rogers.com>
2021-07-30 20:24:21 -0400

    Get movie for user IS DONE

--- src/api/movie/index.js
+++ src/api/movie/index.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { promises } from 'fs';
 import { pool } from '../../db';
 import auth from '../user/auth';
-import { GenreName } from '../../genres';
+import { getMovieForUser } from './getMovieForUser';
 
 const movieRouter = Router();
 
@@ -68,58 +68,27 @@ movieRouter.get('/user/:userid/', auth, async (req, res) => {
         res.status(401).json({});
         return;
     }
-    
 
-    // TODO: Make sure the user has movies not in the record table
+    // Verifying user has movies they can still respond to
+    const amountOfMovies = (await pool.query(`SELECT * FROM movie;`)).rows.length;
+    const amountOfUserRecordEntries = (await pool.query(`SELECT * FROM record WHERE userId = '${userid}';`)).rows.length;
 
-    let sql, requestArray, result;
-    
-    //getting user preferences
-    sql = await promises.readFile(
-        './src/db/sql/user/getUserPrefs.sql',
-        'utf-8',
-    );
-    requestArray = [ userid ];
-    try {
-        result = await pool.query(sql, requestArray);
-    }
-    catch (err) {
-        console.log(err);
-        res.status(400).json({});
+    if (amountOfMovies === amountOfUserRecordEntries) {
+        res.status(400).json({ err: "User has exhausted the Flick database" });
         return;
     }
 
-    
-    console.log(result.rows);
-    let genre_prefs = [];
-    let sum = 0;
-
-    for (let i = 0; i < result.rows.length; i++){
-        genre_prefs.push(result.rows[i].rating);
-        sum += result.rows[i].rating;
+    // Attempt to find a movie up to 50 times, if it can't find it then respond with an error
+    for (let i = 0; i < 50; i += 1) {
+        const movieForUser = await getMovieForUser(userid);
+        if (movieForUser !== false) {
+            res.status(200).json({ movie: movieForUser });
+            return;
+        }
+        console.log(`MovieForUser attempt ${i + 1} FAIL`);
     }
 
-    
-    //select genre to reccomend
-    let random_index = Math.floor(Math.random()*(sum));
-    //console.log(random_index);
-    
-    let counter = 0;
-    let genre_index = -1;
-    //translate selected genre to correct index
-    while(counter <= random_index ){
-        genre_index += 1;
-        counter += genre_prefs[genre_index];
-    }
-
-    //TODO: check if there are movies available in this genre 
-
-
-    //console.log(genre_index);
-    console.log(GenreName[genre_index]);
-
-    
-    res.status(200).json({});
+    res.status(500).json({ err: "Server could not find a movie for the user" });
     return;
 });
 
