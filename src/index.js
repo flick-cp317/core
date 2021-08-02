c6c08e0b2ff6973b5822f3b19b7fb81d763b8fca
Matthew Dibble <mdibble@rogers.com>
2021-08-02 19:53:25 -0400

    CORS

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
-import { userRouter, theatreRouter, recordRouter, movieRouter, notificationRouter} from './api'; 
+import { userRouter, theatreRouter, recordRouter, movieRouter, notificationRouter } from './api'; 
 import { initializeDatabase } from './db';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/user', userRouter);
 app.use('/api/theatre', theatreRouter);
 app.use('/api/record', recordRouter);
