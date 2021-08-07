6351d339a1c4c2d7695976c5d64950f73c52740c
Matthew Dibble <mdibble@rogers.com>
2021-08-07 12:59:41 -0400

    Port

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use('/api/record', recordRouter);
 app.use('/api/movie', movieRouter);
 app.use('/api/notification', notificationRouter);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.listen(port, async () => {
     console.log(`Flick service started [Running on port ${port}]`);
