70bcf8514d8fe4f7addc9baf72eb81a7441c1776
Matthew Dibble <mdibble@rogers.com>
2021-08-07 14:33:36 -0400

    SSL

--- src/db/index.js
+++ src/db/index.js
@@ -8,6 +8,9 @@ const prodConfig = process.env.DATABASE_URL;
 
 const pool = new Pool({
     connectionString: process.env.NODE_ENV === "production" ? prodConfig : devConfig,
+    ssl: {
+        rejectUnauthorized: false,
+    }
 });
 
 async function initializeDatabase() {
